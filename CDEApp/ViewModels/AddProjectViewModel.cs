1dac66517879e96c634be6ce208570ff3253995b
Александр <HellaHoesCity@gmail.com>
2021-04-22 09:00:10 +0700

    Add AddProjectViewModel to make new Projects

---
+++ CDEApp/ViewModels/AddProjectViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+
+namespace CDEApplication.Models.ViewModels
+{
+    public class AddProjectViewModel
+    {
+        #region Properties
+
+        [Required]
+        [Display(Name = "Название проекта")]
+        public string Name { get; set; } //Name of project
+
+        [Required]
+        [Display(Name = "Название документа")]
+        public string DocumentName { get; set; } //Name of document
+
+        [Required]
+        [Display(Name = "Загрузить файл")]
+        public IFormFile Document { get; set; }
+
+        [Required]
+        [Display(Name = "Комментарий")]
+        public string Comment { get; set; }
+        #endregion
+    }
+}
