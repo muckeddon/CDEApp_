187fbb83c9b6af9d20cf0d923c896424d89b821d
Александр <HellaHoesCity@gmail.com>
2021-04-22 08:54:14 +0700

    Add class of Document
    Document class has a Project property for one-to-many relationship.
    Document class has a Comments property for one-to-many relationship.
    Format property is property for save information about file format in DB.

---
+++ CDEApp/Models/Entities/Document.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace CDEApp.Models
+{
+    public class Document
+    {
+        #region Properties
+        public int Id { get; set; }
+        public string Name { get; set; } //Name of document
+        public byte[] Subject { get; set; } //Byte array of file for storage in database
+        public string Format { get; set; } //Format of file
+        public int ProjectId { get; set; }
+        public Project Project { get; set; } //Project for relation
+        public List<Comment> Comments { get; set; } = new List<Comment>(); // List of all comments about document
+        #endregion
+    }
+}
