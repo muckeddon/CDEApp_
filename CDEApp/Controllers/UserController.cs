89fe8b229395f9e7b8e3c28a4d504aa3ff41de49
Александр <HellaHoesCity@gmail.com>
2021-04-22 10:45:13 +0700

    Add UserController that will add user to project.
    AddUser method check for user in DB.
    If User was found, this user add to project, else method call SendEmailAsync method from EmailService class and sending invite-email to email from model.

--- CDEApp/Controllers/UserController.cs
+++ CDEApp/Controllers/UserController.cs
@@ -1,13 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using CDEApp.Models;
+using CDEApp.Models.DataAccessLayer;
+using CDEApp.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace CDEApp.Controllers
 {
-    public class UserController
+    public class UserController : Controller
     {
-        
+        #region Class fields
+
+        ApplicationContext _context;
+        #endregion
+        public UserController(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        #region Methods
+        #region AddUser
+        [HttpGet]
+        public IActionResult AddUser(int projectId)
+        {
+            return View(new AddUserViewModel { ProjectId = projectId });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddUser(AddUserViewModel model)
+        {
+            var user = _context.Users.Where(u => u.Email == model.Email).FirstOrDefault(); //get user that will adding to project by Email from view model
+            if (user is null)
+            {
+                EmailService emailService = new EmailService();
+                await emailService.SendEmailAsync($"{model.Email}", "Вас приглашают присоедениться к проекту Collaborative Document Editing!", "[Ссылка на проект]");
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                var project = _context.Projects.Include(u => u.Users).Where(p => p.Id == model.ProjectId).FirstOrDefault(); // get current project
+                project.Users.Add(user); //add user
+                _context.SaveChanges();
+            }
+            return RedirectToAction("Index", "Home");
+        }
+        #endregion
+        #endregion
     }
 }
