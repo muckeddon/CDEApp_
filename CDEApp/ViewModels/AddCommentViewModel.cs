41d798cda6129ef618f554141966c4df7607af1e
Александр <HellaHoesCity@gmail.com>
2021-04-22 09:05:40 +0700

    Add AddCommentViewModel class for add comment to document

---
+++ CDEApp/ViewModels/AddCommentViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace CDEApp.ViewModels
+{
+    public class AddCommentViewModel
+    {
+        #region Properties
+        public bool IsProjectClose { get; set; } //Project status
+        public int DocumentId { get; set; } //Document ID
+        public List<Document> Documents { get; set; } //List of document in project
+        public string Admin { get; set; } //Admin of project
+        public string CurrentUser { get; set; } //Current user name
+
+        [Required]
+        [Display(Name = "Добавить комментарий к документу:")]
+        public string CommentText { get; set; } //Text of comment
+
+        #endregion
+    }
+}
