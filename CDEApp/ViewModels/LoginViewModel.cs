40db84c66d9f660a4a8bee090720bec885cb4642
Александр <HellaHoesCity@gmail.com>
2021-04-22 08:58:17 +0700

    Add LoginViewModel for User login

---
+++ CDEApp/ViewModels/LoginViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace CDEApp.Models.ViewModels
+{
+    public class LoginViewModel
+    {
+        #region Properties
+
+        [Required]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Пароль")]
+        public string Password { get; set; }
+
+        [Display(Name = "Запомнить?")]
+        public bool RememberMe { get; set; }
+        public string ReturnUrl { get; set; }
+        public IList<AuthenticationScheme> ExternalLogins { get; set; }
+
+        #endregion
+    }
+}
