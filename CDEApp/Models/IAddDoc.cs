c17d782e1581ba18690121f5a38f927b840ffdc7
Александр <HellaHoesCity@gmail.com>
2021-04-22 09:09:33 +0700

    Add IAddDoc interface This interface will implement in ViewModels that can load files.

---
+++ CDEApp/Models/IAddDoc.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace CDEApp.Models
+{
+    public interface IAddDoc
+    {
+        #region Properties
+        public string Name { get; set; } //Name of document
+        public IFormFile Document { get; set; } //File information
+        public string Comment { get; set; } //Text of comment
+        #endregion
+    }
+}
