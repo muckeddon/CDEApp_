c5161bf7c08fcb8035d55d0b9dac8ead1476a53d
Александр <HellaHoesCity@gmail.com>
2021-04-22 08:52:53 +0700

    Add class of user
    User class has a list of Projects for many-to-many relationship.

---
+++ CDEApp/Models/Entities/User.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
+
+namespace CDEApp.Models
+{
+    public class User : IdentityUser
+    {
+        #region Properties
+        public List<Project> Projects { get; set; } = new List<Project>(); //List of projects
+        #endregion
+    }
+}
