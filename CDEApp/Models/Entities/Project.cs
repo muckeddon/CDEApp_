be939e8f1f69a893913c2dfd6d55ec99b234e793
Александр <HellaHoesCity@gmail.com>
2021-04-22 08:53:27 +0700

    Add class of project
    Project class has a list of Users for many-to-many relationship.
    Project class has a list of Documents for one-to-many relationship.

---
+++ CDEApp/Models/Entities/Project.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace CDEApp.Models
+{
+    public class Project
+    {
+        #region Properties
+        public int Id { get; set; }
+        public bool IsClose { get; set; } //Property for understanding to status of project
+        public string Name { get; set; }  //Name of project
+        public List<Document> Documents { get; set; } = new List<Document>(); //List of documents(Files)
+        public List<User> Users { get; set; } = new List<User>(); //List of users who can use this project
+        public string Admin { get; set; } //User who made this project
+        #endregion
+    }
+}
