408197fa2afd98270d559ad274e2019786ab0fa0
Александр <HellaHoesCity@gmail.com>
2021-04-22 08:55:01 +0700

    Add class of comment
    Comment class has a Document propertie for one-to-many relationship.
    Comment class has a Date propertie for save a date and time information about when comment was made.

---
+++ CDEApp/Models/Entities/Comment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CDEApp.Models
+{
+    public class Comment
+    {
+        #region Properties
+        public int Id { get; set; }
+        public string Text { get; set; } //Text of comment
+        public int DocumentId { get; set; } //Id of document which one comment has a relation
+        public User User { get; set; } //User who made comment
+        public DateTime Date { get; set; } //Date and time when comment was made
+        public Document Document { get; set; } //Document(File)
+        #endregion
+    }
+}
