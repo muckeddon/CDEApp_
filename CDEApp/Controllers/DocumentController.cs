16558ecb7ab7c8424c851adc7b60e37801239cd0
Александр <HellaHoesCity@gmail.com>
2021-04-22 10:27:45 +0700

    Add DocumentController
    Add Controller that will create document and download it.
    Add Create method to this controller.

---
+++ CDEApp/Controllers/DocumentController.cs
@@ -0,0 +1,66 @@
+﻿using CDEApp.Models;
+using CDEApp.Models.DataAccessLayer;
+using CDEApp.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CDEApp.Controllers
+{
+    public class DocumentController : Controller
+    {
+        #region Class fields
+
+        ApplicationContext _context;
+        #endregion
+        public DocumentController(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        #region Methods
+
+        #region Create
+        [HttpGet]
+        public IActionResult Create(int projectId)
+        {
+            return View(new AddDocumentViewModel { projectId = projectId });
+        }
+
+        [HttpPost]
+        //Create new document
+        public IActionResult Create(AddDocumentViewModel model)
+        {
+            User user = _context.Users.Include(u => u.Projects).ToList().                       //Get User from DB 
+                Where(u => u.UserName == HttpContext.User.Identity.Name).FirstOrDefault();
+            Project project = _context.Projects.Include(u => u.Documents).ToList().             //Get Project from DB
+                Where(i => i.Id == model.projectId).FirstOrDefault();
+            Document document = new Document();
+            Comment comment = new Comment();
+
+            if (model.Document != null)
+            {
+                byte[] data = null;
+                using (var reader = new BinaryReader(model.Document.OpenReadStream())) //Open stream
+                {
+                    data = reader.ReadBytes((int)model.Document.Length);
+                }
+
+                document = new Document { Project = project, Name = model.Name, ProjectId = project.Id, Subject = data, Format = new FileFormatGetter().GetFileFormat(model) };
+                comment = new Comment { Text = model.Comment, Document = document, DocumentId = document.Id, User = user, Date = DateTime.Now };
+            }
+
+            _context.Comments.Add(comment);   //add new records to tables
+            _context.Documents.Add(document);
+            _context.SaveChanges();           //save db
+            return RedirectToAction("Index", "Home");
+        }
+        #endregion
+        #endregion
+    }
+}
