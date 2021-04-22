d6fe64f396e12c8ada9f5a08ee231b5a91c644fe
Александр <HellaHoesCity@gmail.com>
2021-04-22 09:11:46 +0700

    Add FileFormatGetter class for getting format of load files.

---
+++ CDEApp/Models/FileFormatGetter.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+
+
+namespace CDEApp.Models
+{
+    public class FileFormatGetter
+    {
+        //get model, call a GetPositionFormatDot(model) to get a position of last dot and add to it chars of format. Return string with file format like ".png"
+        public string GetFileFormat(IAddDoc model)
+        {
+            string format = " ";
+
+            for (int i = GetPosionOfFormatDot(model); i < model.Document.FileName.ToCharArray().Count(); i++)
+            {
+                format += model.Document.FileName.ToCharArray()[i];
+            }
+
+            return format;
+        }
+
+        //get model and return a position of a last dot in name of file
+        private int GetPosionOfFormatDot(IAddDoc model)
+        {
+            int position = 0;
+
+            for (int i = 0; i < model.Document.FileName.ToCharArray().Count(); i++)
+            {
+                if (model.Document.FileName.ToCharArray()[i] == '.')
+                {
+                    position = i;
+                }
+            }
+
+            return position;
+        }
+    }
+}
